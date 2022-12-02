9a256c68e30c206636a69503bf27134cb446a138
unknown <miguel.semedo.2@outlook.com>
2022-12-02 08:17:52 +0000

    add user and company marker to map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,13 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+}
+
 export class CustomMap {
 	private googleMap: google.maps.Map;
 
@@ -12,5 +22,15 @@ export class CustomMap {
 				},
 			}
 		);
+	}
+
+	addMarker(mappable: Mappable): void {
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: mappable.location.lat,
+				lng: mappable.location.lng,
+			},
+		});
 	}
 }
